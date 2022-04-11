dafbe5e891e39d10fba209f18b3cdacd5f272bbf
aditya28071999 <aditya28071999@gmail.com>
2022-04-12 00:21:58 +0530

    sidebar changes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Sidebar from '../components/sidebar';
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
       </Head>
       
       <main>
+        <Sidebar>
+          
+        </Sidebar>
       {/*Sidebar */}
       {/*Feed */}
       {/*Widgets */}
